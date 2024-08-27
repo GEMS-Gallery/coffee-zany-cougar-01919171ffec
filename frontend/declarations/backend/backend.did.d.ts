cf947d8d1fea2ece4cab3bdca6b87cf79c02e6dc
sa-gems-github <sa-gems-github@dfinity.org>
2024-08-26 22:10:51 -0400

    01919199-6d68-7bbc-b802-bce428121d9a

--- frontend/declarations/backend/backend.did.d.ts
+++ frontend/declarations/backend/backend.did.d.ts
@@ -2,26 +2,9 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
-export interface RoomConfig {
-  'enable_people_ui' : boolean,
-  'enable_hand_raising' : boolean,
-  'enable_chat' : boolean,
-  'redirect_on_meeting_exit' : [] | [string],
-  'start_with_video_off' : boolean,
-  'enable_prejoin_ui' : boolean,
-  'owner_only_broadcast' : boolean,
-  'enable_emoji_reactions' : boolean,
-  'enable_screenshare' : boolean,
-  'enable_network_ui' : boolean,
-  'close_tab_on_exit' : boolean,
-  'enable_recording' : boolean,
-  'enable_knocking' : boolean,
-}
 export interface _SERVICE {
-  'createRoom' : ActorMethod<[string, RoomConfig], undefined>,
-  'getRoomConfig' : ActorMethod<[string], [] | [RoomConfig]>,
-  'getRoomUrl' : ActorMethod<[string], string>,
-  'updateRoomConfig' : ActorMethod<[string, RoomConfig], undefined>,
+  'getRoomUrl' : ActorMethod<[], string>,
+  'setRoomUrl' : ActorMethod<[string], undefined>,
 }
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
